52c42d657cbfc4fb4a373e002848aa84c1de5d30
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-11-06 13:07:31 +0700

    check-condition-via-use-props-styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import GlobalStyles from "./styles/GlobalStyles.js"
 import Button from "./ui/Button.jsx"
 import Input from "./ui/input.jsx"
 import Heading from "./ui/Heading.jsx"
+import Row from "./ui/Row.jsx"
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `
 
@@ -15,15 +16,31 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The wild Oasis</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The wild Oasis</Heading>
 
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check out</Button>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="medium"
+                onClick={() => alert("Check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   )
