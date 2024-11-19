5486516cc62f946e88675400abe64080e6140384
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-11-19 13:35:25 +0700

    make-today-acticities

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -15,6 +15,7 @@ import Stats from "./Stats"
 import { useCabins } from "../cabins/useCabins"
 import SalesChart from "./SalesChart"
 import DurationChart from "./DurationChart"
+import TodayActivity from "../check-in-out/TodayActivity"
 
 export default function DashboardLayout() {
   const { isLoading: isLoading1, bookings } = useRecentBookings()
@@ -31,7 +32,7 @@ export default function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today's activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
