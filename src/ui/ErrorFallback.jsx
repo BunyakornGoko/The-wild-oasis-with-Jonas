49780388d25d00e55cf5d95b04351339e05cb760
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-11-19 13:59:42 +0700

    make-error-boundary

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -7,7 +7,7 @@ const StyledErrorFallback = styled.main`
   align-items: center;
   justify-content: center;
   padding: 4.8rem;
-`;
+`
 
 const Box = styled.div`
   /* Box */
@@ -28,4 +28,26 @@ const Box = styled.div`
     margin-bottom: 3.2rem;
     color: var(--color-grey-500);
   }
-`;
+`
+
+import React from "react"
+import Heading from "./Heading"
+import GlobalStyles from "../styles/GlobalStyles"
+import Button from "./Button"
+
+export default function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 😎</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  )
+}
